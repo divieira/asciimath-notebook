785b01de5a982d9401dd8a498fc527c2b5f88326
Diego Vieira <divieira@gmail.com>
2016-10-13 00:26:09 +0200

    Add comments

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime as dt
 import logging
 
+# Initialize Heroku app and database connection
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -13,43 +14,67 @@ db = SQLAlchemy(app)
 app.debug = True
 
 
+# SQLAlchemy Database entity for a note
+# (each class corresponds to a database table and each instance to a table row)
 class Note(db.Model):
     '''ORM mapping of the Note entity'''
+    # Each attribute is a table column
+    # These attributes are created automatically
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     modified_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
+    # These attributes should be set manually (default: None)
     title = db.Column(db.String)
     content = db.Column(db.String)
 
 
+# Flask URL handlers
 @app.route('/')
 def index():
+    '''Index page'''
+
+    # Query all instances of Note from database
     notes = Note.query.all()
+
+    # Render index page listing all notes
     return render_template('index.html', notes=notes)
 
 
 @app.route('/note/new')
 def note_new():
+    '''Create a new note'''
+
+    # Create a new instance from Note and add it to database
     note = Note()
     db.session.add(note)
     db.session.commit()
+
+    # Redirect user to newly created note
     return redirect('/note/%d' % note.id)
 
 
 @app.route('/note/<int:note_id>', methods=['GET', 'POST'])
 def note(note_id):
+    '''Open/update an existing note'''
+
+    # Retrieve an existing instance from Note by id
     note = Note.query.get_or_404(note_id)
 
     if request.method == 'POST':
+        # If responding to a POST, update fields from HTML form
         note.title = request.form.get('title')
         note.content = request.form.get('content')
         note.modified_at = dt.datetime.now()
+
+        # And commit instance changes to database
         db.session.add(note)
         db.session.commit()
 
+    # Render note page containing an HTML form with instance contents
     return render_template('note.html', note=note)
 
 
+# Run app from command-line
 if __name__ == '__main__':
     app.run()
